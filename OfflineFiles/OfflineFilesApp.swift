35b89e7da1dbf1e0e15707a7b891336a9ebadb7c
Abith R <abithrha@gmail.com>
2025-01-08 13:27:13 +0530

    Initial Commit

---
+++ OfflineFiles/OfflineFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineFilesApp.swift
+//  OfflineFiles
+//
+//  Created by Abith Rajakumaran on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct OfflineFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
